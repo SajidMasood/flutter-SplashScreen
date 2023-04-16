5bdfd55c4572bf91205049dd3008f733ccc72768
Sajid Masood <sajidullah_m@yahoo.com>
2023-04-16 20:28:51 +0500

    completed

---
+++ lib/utils/AppColors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color primarygreen = Color(0xFF1E3A34);
+  static final Color lightgreenshede = Color(0xFFF0FAF6);
+  static final Color greenshede0 = Color(0xFF66A690);
+  static final Color greenshede1 = Color(0xFF93C9B5);
+  static final Color grayshade = Color(0xFF93B3AA);
+  static final Color colorAcent = Color(0xFF78C2A7);
+  static final Color cyanColor = Color(0xFF6D7E6E);
+  static final Color lightgreenshede1 = Color(0xFFB2D9CC);
+
+  
+static final  kAnimationDuration = Duration(milliseconds: 200);
+}
